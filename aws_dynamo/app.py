50883f72650f6fbf0dd46305011ef377da4c37fb
Kevin Aspegren <kevin.aspegren@student.kyh.se>
2021-02-24 15:48:00 +0100

    Added protocol and changes in code

--- aws_dynamo/app.py
+++ aws_dynamo/app.py
@@ -6,13 +6,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    main_door_1 = get_all_by_sensorname('main_door_1')
+    return render_template('index.html', main_door_1=main_door_1)
 
 @app.route('/load')
 def load():
     main_door_1 = get_all_by_sensorname('main_door_1')
     main_door_2 = get_all_by_sensorname('main_door_2')
     return render_template('load.html', main_door_1=main_door_1, main_door_2=main_door_2)
+
+@app.route('/someoneoutside')
+def someoneoutside():
+    return render_template('someoneoutside.html')
+
+@app.route('/nooneoutside')
+def nooneoutside():
+    return render_template('nooneoutside.html')
 
 
 @app.route('/sensors')
