a79387a80c8f36e5930bf61fcd4954967f8586df
Kevin Aspegren <kevin.aspegren@student.kyh.se>
2021-03-05 13:29:52 +0100

    Fixed css

--- aws_dynamo/app.py
+++ aws_dynamo/app.py
@@ -11,15 +11,18 @@ def index():
     main_door_1 = get_all_by_sensorname('main_door_1')
     return render_template('index.html', main_door_1=main_door_1)
 
+
 @app.route('/load')
 def load():
     main_door_1 = get_all_by_sensorname('main_door_1')
     main_door_2 = get_all_by_sensorname('main_door_2')
     return render_template('load.html', main_door_1=main_door_1, main_door_2=main_door_2)
 
+
 @app.route('/someoneoutside')
 def someoneoutside():
     return render_template('someoneoutside.html')
+
 
 @app.route('/nooneoutside')
 def nooneoutside():
@@ -30,7 +33,7 @@ def nooneoutside():
 def get_sensors():
     main_door_1 = get_all_by_sensorname('main_door_1')
     main_door_2 = get_all_by_sensorname('main_door_2')
-    return render_template('sensors.html',main_door_1=main_door_1, main_door_2=main_door_2)
+    return render_template('sensors.html', main_door_1=main_door_1, main_door_2=main_door_2)
 
 
 @app.route('/check')
